323bf1a61edfd3da5f8418ba6014236d2d3d04ad
Abzed <ibraabzed@gmail.com>
2022-04-10 16:07:48 +0300

    commit tree 3a17e7356f5052bd46d3253c6daf2bb33938d633

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, str){
+    let filterDrivers = []
+    for(const driver of drivers){
+        if(driver.toUpperCase() == str.toUpperCase()){
+            filterDrivers.push(driver)
+        }
+    }
+    return filterDrivers
+}
+
+console.log(findMatching(drivers,'Bobby'));
+
+function fuzzyMatch(drivers, str){
+    let matches = []
+    for (let driver of drivers) {
+        if(String(driver).substring(0,3).match(str)){
+            matches.push(driver)
+        }
+    }
+    return matches;
+}
+console.log(fuzzyMatch(drivers, "B"));
+
+const drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(drivers2, str){
+    let finalArr = []
+    for(let driver of drivers2){
+        if(driver.name == str){
+            finalArr.push(driver)
+        }
+    }
+    return finalArr
+}
+console.log(matchName(drivers2, "Bobby"));
+
+
+
+
